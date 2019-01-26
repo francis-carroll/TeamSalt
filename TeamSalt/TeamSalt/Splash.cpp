9cd569c9e7aefbe6f40013f3da6d36e20fc48b89
ClionaHayden <c00225401@itcarlow.ie>
2019-01-26 09:40:39 +0000

    started splash

--- TeamSalt/TeamSalt/Splash.cpp
+++ TeamSalt/TeamSalt/Splash.cpp
@@ -1,1 +1,17 @@
 #include "Splash.h"
+
+SplashScreen::SplashScreen()
+{
+}
+
+SplashScreen::~SplashScreen()
+{
+}
+
+void SplashScreen::draw(sf::RenderWindow & t_window)
+{
+}
+
+void SplashScreen::update(sf::Time dt)
+{
+}
--- TeamSalt/TeamSalt/Splash.h
+++ TeamSalt/TeamSalt/Splash.h
@@ -1,7 +1,16 @@
 
-
+#include "SFML/Graphics.hpp"
 class SplashScreen
 {
 public:
+	SplashScreen();
+	~SplashScreen();
+
+	void draw(sf::RenderWindow &t_window);
+	void update(sf::Time dt);
 private:
+	sf::Sprite m_background;
+	sf::Sprite m_player;
+	sf::Font m_font;
+	sf::Text m_continueText;
 };
