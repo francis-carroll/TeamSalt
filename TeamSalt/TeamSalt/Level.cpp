3809c9bdea048e6f9532136f4a65e8298d28ed30
ClionaHayden <c00225401@itcarlow.ie>
2019-01-26 15:57:25 +0000

    started level class

---
+++ TeamSalt/TeamSalt/Level.cpp
@@ -0,0 +1,29 @@
+#include "Level.h"
+
+Level::Level()
+{
+}
+
+Level::~Level()
+{
+}
+
+void Level::initialise(sf::Font & t_font)
+{
+}
+
+void Level::render(sf::RenderWindow & t_window)
+{
+}
+
+void Level::update(sf::Time dt)
+{
+}
+
+void Level::setupSprite()
+{
+}
+
+void Level::setupText()
+{
+}
