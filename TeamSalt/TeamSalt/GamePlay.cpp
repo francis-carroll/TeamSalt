150ecff2cf695913707b4978588b5c5742c18842
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2019-01-27 13:29:09 +0000

    removed enemy

--- TeamSalt/TeamSalt/GamePlay.cpp
+++ TeamSalt/TeamSalt/GamePlay.cpp
@@ -13,7 +13,7 @@ void GamePlay::initialise(sf::Font & t_font)
 {
 	m_level1.initialise(t_font);
 	m_player.init();
-	m_enemy.init();
+//	m_enemy.init();
 }
 
 void GamePlay::render(sf::RenderWindow & t_window)
@@ -22,7 +22,7 @@ void GamePlay::render(sf::RenderWindow & t_window)
 	t_window.clear();
 	m_level1.render(t_window);
 	m_player.draw(t_window);
-	m_enemy.draw(t_window);
+//	m_enemy.draw(t_window);
 	t_window.display();
 }
 
@@ -30,7 +30,7 @@ void GamePlay::update(sf::Time dt)
 {
 	m_level1.update(dt);
 	m_player.update(dt);
-	m_enemy.update();
+//	m_enemy.update();
 	checkPlayerCollision();
 	m_view.setCenter(m_player.getPos());
 
@@ -48,14 +48,7 @@ void GamePlay::checkPlayerCollision()
 {
 	for (int i = 0; i < MAX_TILES; i++)
 	{
-		/*if (m_player.getPos().x - (m_player.getSprite().getGlobalBounds().width / 2) < m_level1.m_groundTiles[i].getPosition().x)
-		{
-			m_player.setPos(m_player.getPos() + sf::Vector2f{ 0.05f,0.0f });
-		}
-		else if (m_player.getPos().x + (m_player.getSprite().getGlobalBounds().width / 2) > m_level1.m_groundTiles[i].getPosition().x)
-		{
-			m_player.setPos(m_player.getPos() - sf::Vector2f{ 0.05f,0.0f });
-		}*/
+		
 		if (m_player.getSprite().getGlobalBounds().intersects(m_level1.m_groundTiles[i].getSprite().getGlobalBounds()))
 		{
 			m_player.isOnGround(m_player.getPos());
@@ -66,13 +59,13 @@ void GamePlay::checkPlayerCollision()
 			m_player.isFalling();
 		}
 
-		if (m_enemy.getSprite().getGlobalBounds().intersects(m_level1.m_groundTiles[i].getSprite().getGlobalBounds()))
+	/*	if (m_enemy.getSprite().getGlobalBounds().intersects(m_level1.m_groundTiles[i].getSprite().getGlobalBounds()))
 		{
 			m_enemy.isOnGround();
 		}
 		else
 		{
 			m_enemy.falling();
-		}
+		}*/
 	}
 }
